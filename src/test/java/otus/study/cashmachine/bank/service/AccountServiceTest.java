a52f8044725142454db9c471f75fcb9aea49f122
Stern-Ritter <fadeew.a@mail.ru>
2023-07-10 09:39:06 -0400

    Add unit tests

--- src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
+++ src/test/java/otus/study/cashmachine/bank/service/AccountServiceTest.java
@@ -1,40 +1,123 @@
 package otus.study.cashmachine.bank.service;
 
-
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatcher;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import otus.study.cashmachine.bank.dao.AccountDao;
+import otus.study.cashmachine.bank.data.Account;
 import otus.study.cashmachine.bank.service.impl.AccountServiceImpl;
 
+import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
 public class AccountServiceTest {
 
-    AccountDao accountDao;
+    @Mock
+    private AccountDao accountDao;
 
-    AccountServiceImpl accountServiceImpl;
+    @InjectMocks
+    private AccountServiceImpl accountService;
 
     @Test
     void createAccountMock() {
-// @TODO test account creation with mock and ArgumentMatcher
+        // @TODO test account creation with mock and ArgumentMatcher
+        long defaultId = 0;
+        BigDecimal amount = BigDecimal.valueOf(1000);
+        accountService.createAccount(amount);
+
+        ArgumentMatcher<Account> matcher = argument ->
+                argument.getId() == defaultId && amount.equals(argument.getAmount());
+        verify(accountDao).saveAccount(argThat(matcher));
     }
 
     @Test
     void createAccountCaptor() {
-//  @TODO test account creation with ArgumentCaptor
+        // @TODO test account creation with ArgumentCaptor
+        long defaultId = 0;
+        BigDecimal amount = BigDecimal.valueOf(1000);
+        accountService.createAccount(amount);
+
+        ArgumentCaptor<Account> accountArgumentCaptor = ArgumentCaptor.forClass(Account.class);
+        verify(accountDao).saveAccount(accountArgumentCaptor.capture());
+
+        Account account = accountArgumentCaptor.getValue();
+        assertEquals(defaultId, account.getId());
+        assertEquals(amount, account.getAmount());
     }
 
     @Test
-    void addSum() {
+    void putMoney() {
+        long accountId = 1;
+        BigDecimal amount = BigDecimal.valueOf(100);
+        BigDecimal addedAmount = BigDecimal.valueOf(200);
+        Account savedAccount = new Account(accountId, amount);
+        when(accountDao.getAccount(accountId)).thenReturn(savedAccount);
+
+        BigDecimal updatedAmount = accountService.putMoney(accountId, addedAmount);
+        BigDecimal expectedAmount = amount.add(addedAmount);
+        assertEquals(expectedAmount, updatedAmount);
     }
 
     @Test
-    void getSum() {
+    void getMoneyWithEnoughMoney() {
+        long accountId = 1;
+        BigDecimal amount = BigDecimal.valueOf(100);
+        BigDecimal subtractedAmount = BigDecimal.valueOf(100);
+
+        Account savedAccount = new Account(accountId, amount);
+        when(accountDao.getAccount(accountId)).thenReturn(savedAccount);
+
+        BigDecimal updatedAmount = accountService.getMoney(accountId, subtractedAmount);
+        BigDecimal expectedAmount = amount.subtract(subtractedAmount);
+        assertEquals(expectedAmount, updatedAmount);
+    }
+
+    @Test
+    void getMoneyWithNotEnoughMoney() {
+        long accountId = 1;
+        BigDecimal amount = BigDecimal.valueOf(100);
+        BigDecimal subtractedAmount = BigDecimal.valueOf(101);
+
+        Account savedAccount = new Account(accountId, amount);
+        when(accountDao.getAccount(accountId)).thenReturn(savedAccount);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> accountService.getMoney(accountId, subtractedAmount));
+
+        String expectedMessage = "Not enough money";
+        assertEquals(expectedMessage, exception.getMessage());
     }
 
     @Test
     void getAccount() {
+        long accountId = 1;
+        BigDecimal amount = BigDecimal.valueOf(100);
+        Account savedAccount = new Account(accountId, amount);
+        when(accountDao.getAccount(accountId)).thenReturn(savedAccount);
+
+        Account account = accountService.getAccount(accountId);
+        assertEquals(savedAccount, account);
     }
 
     @Test
     void checkBalance() {
+        long accountId = 1;
+        BigDecimal amount = BigDecimal.valueOf(100);
+        Account savedAccount = new Account(accountId, amount);
+        when(accountDao.getAccount(accountId)).thenReturn(savedAccount);
+
+        BigDecimal savedAmount = accountService.checkBalance(accountId);
+        assertEquals(amount, savedAmount);
     }
 }
--- src/test/java/otus/study/cashmachine/machine/service/CashMachineServiceTest.java
+++ src/test/java/otus/study/cashmachine/machine/service/CashMachineServiceTest.java
@@ -3,7 +3,10 @@ package otus.study.cashmachine.machine.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import otus.study.cashmachine.bank.dao.CardsDao;
 import otus.study.cashmachine.bank.service.AccountService;
@@ -12,53 +15,141 @@ import otus.study.cashmachine.machine.data.CashMachine;
 import otus.study.cashmachine.machine.data.MoneyBox;
 import otus.study.cashmachine.machine.service.impl.CashMachineServiceImpl;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @ExtendWith(MockitoExtension.class)
 class CashMachineServiceTest {
+
+    @Mock
+    private AccountService accountService;
+
+    @Mock
+    private CardsDao cardsDao;
 
     @Spy
     @InjectMocks
     private CardServiceImpl cardService;
 
     @Mock
-    private CardsDao cardsDao;
-
-    @Mock
-    private AccountService accountService;
-
-    @Mock
     private MoneyBoxService moneyBoxService;
+
+    private CashMachine cashMachine;
 
     private CashMachineServiceImpl cashMachineService;
 
-    private CashMachine cashMachine = new CashMachine(new MoneyBox());
-
     @BeforeEach
-    void init() {
+    void setUp() {
+        cashMachine = new CashMachine(new MoneyBox());
         cashMachineService = new CashMachineServiceImpl(cardService, accountService, moneyBoxService);
     }
 
-
     @Test
     void getMoney() {
-// @TODO create get money test using spy as mock
+        // @TODO create get money test using spy as mock
+        String cardNum = "1111";
+        String pinCode = "1011";
+        BigDecimal amount = BigDecimal.valueOf(1000);
+        List<Integer> expectedNotes = List.of(1, 1, 1, 1);
+
+        doReturn(amount).when(cardService).getMoney(cardNum, pinCode, amount);
+        doReturn(expectedNotes).when(moneyBoxService).getMoney(cashMachine.getMoneyBox(), amount.intValue());
+
+        List<Integer> notes = cashMachineService.getMoney(cashMachine, cardNum, pinCode, amount);
+
+        verify(cardService, times(1)).getMoney(cardNum, pinCode, amount);
+        verify(cardService, never()).putMoney(anyString(), anyString(), any(BigDecimal.class));
+        verify(moneyBoxService, times(1)).getMoney(cashMachine.getMoneyBox(), amount.intValue());
+        assertIterableEquals(expectedNotes, notes);
     }
 
     @Test
     void putMoney() {
+        String cardNum = "1111";
+        String pinCode = "1011";
+        int note100 = 1;
+        int note500 = 1;
+        int note1000 = 1;
+        int note5000 = 1;
+
+        List<Integer> notes = List.of(note5000, note1000, note500, note100);
+        BigDecimal expectedAmount = BigDecimal.valueOf(
+                note100 * 100 + note500 * 500 + note1000 * 1000 + note5000 * 5000);
+
+        doReturn(BigDecimal.ZERO).when(cardService).getBalance(cardNum, pinCode);
+
+        doReturn(expectedAmount).when(cardService).putMoney(cardNum, pinCode, expectedAmount);
+
+        BigDecimal amount = cashMachineService.putMoney(cashMachine, cardNum, pinCode, notes);
+
+        verify(moneyBoxService, times(1)).putMoney(
+                cashMachine.getMoneyBox(), note100, note500, note1000, note5000);
+        verify(moneyBoxService, never()).getMoney(any(MoneyBox.class), anyInt());
+        verify(cardService, times(1)).putMoney(cardNum, pinCode, expectedAmount);
+        assertEquals(expectedAmount, amount);
     }
 
     @Test
     void checkBalance() {
+        String cardNum = "1111";
+        String pinCode = "1011";
+        BigDecimal expectedAmount = BigDecimal.ONE;
+        doReturn(expectedAmount).when(cardService).getBalance(cardNum, pinCode);
 
+        BigDecimal amount = cashMachineService.checkBalance(cashMachine, cardNum, pinCode);
+
+        verify(cardService, times(1)).getBalance(cardNum, pinCode);
+        assertEquals(expectedAmount, amount);
     }
 
     @Test
     void changePin() {
-// @TODO create change pin test using spy as implementation and ArgumentCaptor and thenReturn
+        // @TODO create change pin test using spy as implementation and ArgumentCaptor and thenReturn
+        String cardNum = "1111";
+        String oldPinCode = "1011";
+        String newPinCode = "0100";
+
+        ArgumentCaptor<String> cardNumCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> oldPinCodeCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> newPinCodeCaptor = ArgumentCaptor.forClass(String.class);
+
+        doReturn(true)
+                .when(cardService).cnangePin(cardNumCaptor.capture(), oldPinCodeCaptor.capture(), newPinCodeCaptor.capture());
+
+        boolean result = cashMachineService.changePin(cardNum, oldPinCode, newPinCode);
+
+        assertEquals(cardNum, cardNumCaptor.getValue());
+        assertEquals(oldPinCode, oldPinCodeCaptor.getValue());
+        assertEquals(newPinCode, newPinCodeCaptor.getValue());
+        assertTrue(result);
     }
 
     @Test
     void changePinWithAnswer() {
-// @TODO create change pin test using spy as implementation and mock an thenAnswer
+        // @TODO create change pin test using spy as implementation and mock an thenAnswer
+        String cardNum = "1111";
+        String oldPinCode = "1011";
+        String newPinCode = "0100";
+
+        doAnswer(invocation -> oldPinCode.equals(newPinCode))
+                .when(cardService).cnangePin(cardNum, oldPinCode, newPinCode);
+
+        boolean result = cashMachineService.changePin(cardNum, oldPinCode, newPinCode);
+
+        verify(cardService, times(1)).cnangePin(cardNum, oldPinCode, newPinCode);
+        assertFalse(result);
     }
 }
